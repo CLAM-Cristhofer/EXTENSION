0da8e7d53ced546c4619c83157a43583dbe22f33
CLAM-Cristhofer <cristhoferarismendizmogollon@gmail.com>
2024-11-04 11:34:31 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace area {
+    //% block= "circle radius $radius"
+    //% radius.defl-100
+    //% radius.min-0 radius.max-1000
+    export function circulo(radius:number) {
+        return Math.PI * radius * radius;
+    }
+    //% block"rectangle length $length width Swidth"
+    export function rectangle(length: number, width: number) {
+        return length * width;
+    }
+    //% block-"square length Slength"
+    export function square(length: number){
+          return length * length;
+    }
+      
+
+    // block-"triangle base $base heigth $heigth"
+    export function triangle(base: number, heigth: number) {
+        return 0.5 * base * heigth;
+    }
+}
+
