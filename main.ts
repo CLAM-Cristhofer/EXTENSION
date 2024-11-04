e23236921ac677ffcd678d45470645f7312911c7
CLAM-Cristhofer <cristhoferarismendizmogollon@gmail.com>
2024-11-04 11:38:53 -0500

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -13,17 +13,18 @@ namespace area {
     export function circulo(radius:number) {
         return Math.PI * radius * radius;
     }
-    //% block"rectangle length $length width Swidth"
+
+    //% block"rectangle length $length width $width"
     export function rectangle(length: number, width: number) {
         return length * width;
     }
-    //% block-"square length Slength"
+    //% block="square length $length"
     export function square(length: number){
           return length * length;
     }
       
 
-    // block-"triangle base $base heigth $heigth"
+    //% block="triangle base $base heigth $heigth"
     export function triangle(base: number, heigth: number) {
         return 0.5 * base * heigth;
     }
